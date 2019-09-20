34c10004486213c9d22db8f9eda7c860b87e95aa
Francois Grandsire <grandsiref@gmail.com>
2019-09-20 15:57:31 +0200

    fin vendredi

--- create_user.php
+++ create_user.php
@@ -6,33 +6,38 @@ use App\Model\UserModel;
 use App\Service\Avatar\SvgAvatarFactory;
 use App\Service\Helpers\FileSystemHelper;
 use App\Entity\User;
+use Symfony\Component\Dotenv\Dotenv;
 
-//const DB_HOST = 'localhost';
-//const DB_NAME = 'avatarpixel';
-//const DB_USER = 'root';
-//const DB_PASSWORD = '';
+//on vas chercher dans le fichier .env les informations de connection a la bdd
+$dotenv=new Dotenv();
+$dotenv->load(__DIR__.'/.env');
+$config=explode('##',$_ENV['DATABASE']);
+dump($config);
 
-//dump($_POST);
+//si la variable globale POST n'est pas vide on se connecte a la base de données
 if(!empty($_POST)){
 
     //connection a la base de données par injection
-    $pdo = new PDO ('mysql:host=localhost;dbname=avatarpixel','root', '', [PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION]);
+    $pdo = new PDO ('mysql:host='.$config[0].';dbname='.$config[1],$config[2], $config[3], [PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION]);
     $pdo->exec('SET NAMES UTF8');
     $db=new Database($pdo);
 
     //on indique le chemin des fichier pour stocker le fichier svg
     $folders=['uploads','avatars'];
 
+    // constructeur de svg
     $svg=SvgAvatarFactory::getAvatar(3,7);
 
+    // On donne un nom aléatoire au fichier svg
     $filename=sha1(uniqid(rand(),true));
 
+    //on instancie la classe File system pour creer le fichier en .svg
     $fs=new FileSystemHelper();
-
-
     $fs->write('uploads/avatars/'.$filename.'.svg',$svg);
 
+    //On
     $userModel= new UserModel($db);
+
     try{
         $user=new User($_POST);
         $user->setAvatar($filename);
